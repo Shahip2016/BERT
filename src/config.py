a282ec44a9c318724ffbefa89e27acda4fbd4e01
shahip2016 <shahip2016@gmail.com>
2026-01-19 23:27:29 +0530

    Add BERT configuration class

---
+++ src/config.py
@@ -0,0 +1,42 @@
+
+class BertConfig:
+    def __init__(
+        self,
+        vocab_size=30522,
+        hidden_size=768,
+        num_hidden_layers=12,
+        num_attention_heads=12,
+        intermediate_size=3072,
+        hidden_act="gelu",
+        hidden_dropout_prob=0.1,
+        attention_probs_dropout_prob=0.1,
+        max_position_embeddings=512,
+        type_vocab_size=2,
+        initializer_range=0.02,
+        layer_norm_eps=1e-12,
+        pad_token_id=0,
+    ):
+        self.vocab_size = vocab_size
+        self.hidden_size = hidden_size
+        self.num_hidden_layers = num_hidden_layers
+        self.num_attention_heads = num_attention_heads
+        self.hidden_act = hidden_act
+        self.intermediate_size = intermediate_size
+        self.hidden_dropout_prob = hidden_dropout_prob
+        self.attention_probs_dropout_prob = attention_probs_dropout_prob
+        self.max_position_embeddings = max_position_embeddings
+        self.type_vocab_size = type_vocab_size
+        self.initializer_range = initializer_range
+        self.layer_norm_eps = layer_norm_eps
+        self.pad_token_id = pad_token_id
+
+    @classmethod
+    def from_dict(cls, json_object):
+        config = BertConfig(vocab_size=None)
+        for key, value in json_object.items():
+            config.__dict__[key] = value
+        return config
+
+    def to_dict(self):
+        output = copy.deepcopy(self.__dict__)
+        return output
